028a79d318bcbe4b76e3c61bf8375bf78f49b224
Dylan Cronk <cronkyyy@gmail.com>
2023-04-03 00:48:00 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Dylan Cronk on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
